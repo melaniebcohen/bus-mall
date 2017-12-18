bd4c65f47fd47aca2455a977f55734ca2f2876b7
Melanie Cohen <melaniebcohen@gmail.com>
2017-12-18 13:17:44 -0800

    randomized image showing up

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ function Product(filepath) {
   Product.allProducts.push(this);
 };
 
-// find a way to loop through the img folder?
+// Instantiate objects - find a way to loop through the img folder instead?
 new Product('img/bag.png');
-new Product('img/banana.png');
+new Product('img/banana.jpg');
 new Product('img/bathroom.jpg');
 new Product('img/boots.jpg');
 new Product('img/breakfast.jpg');
@@ -31,5 +31,82 @@ new Product('img/tauntaun.jpg');
 new Product('img/unicorn.jpg');
 new Product('img/usb.gif');
 new Product('img/water-can.jpg');
-new Product('img/wine-glass.png');
+new Product('img/wine-glass.jpg');
+
 console.log(Product.allProducts);
+
+// Generate random number
+function getRandomIntInclusive(min, max) { // from MDN
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Pull product at randomized index number
+function displayRandomProduct() {
+  return (getRandomIntInclusive(1, Product.allProducts.length) - 1);
+};
+
+displayRandomProduct();
+
+// Push product to DOM
+function renderProduct() {
+  var images = document.getElementById('images');
+  var imgEl = document.createElement('img');
+  imgEl.src = Product.allProducts[displayRandomProduct()].filepath;
+  images.appendChild(imgEl);
+};
+
+function pushRandomProduct() {
+  if (Product.allProducts[displayRandomProduct()].previouslyShown === false) {
+    // push image into DOM
+    renderProduct();
+    console.log('yep');
+  } else {
+    console.log('nope');
+  }
+  // if Product.allProducts.previouslyShown is false - SHOW
+};
+pushRandomProduct();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// foo
